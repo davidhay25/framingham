573309e9b543e6219a999079a068ec8be5b51608
David Hay <david.hay25@gmail.com>
2018-08-15 15:00:13 +1200

    working on arcsdemo

--- js/arcsdemo.js
+++ js/arcsdemo.js
@@ -3,10 +3,40 @@ angular.module("demoApp",[])
         function ($scope,$http) {
 
             $scope.log = [];
-            $scope.state = 'search'
-            $scope.input = {};
+            $scope.state = 'search';
+            $scope.input = {new:{}};
             $scope.input.name = 'hay';
             $scope.serverUrl = "http://snapp.clinfhir.com:8081/baseDstu3/";
+
+            $scope.saveAE = function(){
+                var ae = {resourceType:'AdverseEvent'};
+                ae.subject={reference:'Patient/'+$scope.patient.id};
+                ae.description = $scope.input.new.description;
+                var date = moment($scope.input.new.date).format();
+
+                ae.date =moment($scope.input.new.date).format();
+                console.log(ae)
+
+                var url = $scope.serverUrl + "AdverseEvent";
+                $scope.log.push({method:'POST',url:"AdverseEvent"});
+                $http.post(url,ae).then(
+                    function(data) {
+                        alert('Adverse Event has been saved')
+
+                        //add to the local list of Adverse Events so that the display is updated
+                        if ($scope.adverseEvents) {
+                            $scope.adverseEvents.entry = $scope.adverseReactions.entry || []
+                            $scope.adverseEvents.entry.push({resource:ae})
+                        }
+
+                    },
+                    function(err) {
+                        alert('There was an error: '+angular.toJson(ae))
+                    }
+                )
+
+
+            };
 
             $scope.selectEntry = function(entry){
                 $scope.patient = entry.resource;
@@ -16,32 +46,29 @@ angular.module("demoApp",[])
                 $scope.log.push({method:'GET',url:"AdverseEvent?subject=Patient/"+$scope.patient.id})
                 $http.get(url).then(
                     function(data) {
-                        $scope.adverseReactions = data.data;
-                        console.log($scope.adverseReactions)
+                        $scope.adverseEvents = data.data;
+                        console.log($scope.adverseEvents)
                     }, function(err) {
                         alert(err.data)
                     }
                 )
-
-
             };
 
             $scope.showLog = function() {
                 $scope.state = 'showlog';
-            }
+            };
 
             $scope.searchPatient = function(){
                 delete $scope.patient;
                 delete $scope.adverseReactions;
                 $scope.state = 'search'
-            }
+            };
 
             $scope.findPatient = function(name) {
                 var url = $scope.serverUrl + "Patient?name="+name;
                 $scope.log.push({method:'GET',url:"Patient?name="+name})
                 $http.get(url).then(
                     function(data) {
-
                         $scope.patients = data.data;
                         console.log($scope.patients)
                     }, function(err) {
@@ -49,4 +76,4 @@ angular.module("demoApp",[])
                     }
                 )
             }
-        })
+        });
