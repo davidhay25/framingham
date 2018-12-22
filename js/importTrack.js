b6560f20d29dbd79202005ea7c26afc04c788318
davidhay25 <david.hay25@gmail.com>
2018-12-23 08:43:52 +1300

    working on import

--- js/importTrack.js
+++ js/importTrack.js
@@ -30,9 +30,32 @@ angular.module("sampleApp")
                             track.leads = [];
                             track.scenarioIds = [];
                             track.id = 'id' + new Date().getTime();
+                            let roleSuffix = '-' + new Date().getTime();    //to ensure roleId is unique...
 
                             //re-identify scenarios...
                             if (track.scenarios) {
+                                //roles are in a separate collection references by _id. scenario.roles is a cache...
+                                hashRole = {};      //all roles for all scenarios in this track
+                                track.scenarios.forEach(function (scenario,inx) {
+                                    if (scenario.roles) {
+                                        scenario.roleIds = [];
+                                        scenario.roles.forEach(function (role) {
+                                           // delete role._id;
+                                            hashRole[role.id] = role;
+                                            scenario.roleIds.push(role.id + roleSuffix)     //update the id
+                                        });
+                                        delete scenario.roles;      //this is a cache
+                                    }
+                                });
+
+                                //save all the roles
+                                angular.forEach(hashRole,function(role){
+                                    role.id = role.id + roleSuffix;
+                                    queries.push(addEntity('role',role));
+                                });
+
+
+
                                 $scope.progress.push('Saving scenarios')
 
                                 track.scenarios.forEach(function (scenario,inx) {
@@ -40,6 +63,9 @@ angular.module("sampleApp")
                                     delete scenario._id;
                                     track.scenarioIds.push(scenario.id);
 
+
+                                    queries.push(addEntity('scenario',scenario));
+                                    /*
                                     //save the scenario here
                                     queries.push(new function(){
                                         let deferred = $q.defer();
@@ -55,20 +81,23 @@ angular.module("sampleApp")
                                         )
                                         return deferred.promise
                                     });
-
-
-                                    //------
-
-
+                                    */
 
                                 })
 
                             }
 
                             //save the track
+                            delete track.scenarios;     //the scenario objects are like a cache. scenarioIds are what count...
+                            //
+                            //
+                            // delete track._id;
+                            queries.push(addEntity('track',track));
+                            /*
+
                             queries.push(new function(){
-                                delete track.scenarios;     //the scenario objects are like a cache. scenarioIds are what count...
-                                delete track._id;
+
+
                                 let deferred = $q.defer();
                                 let url = "/config/track";
                                 $http.post(url,track).then(
@@ -83,6 +112,7 @@ angular.module("sampleApp")
                                 return deferred.promise
 
                             });
+                            */
 
 console.log(queries)
                             //process all the queries
@@ -99,7 +129,25 @@ console.log(queries)
 
 
                             //$scope.$close(svr);
+                        };
+
+                        function addEntity(type,obj) {
+                            let deferred = $q.defer();
+                            let url = "/config/"+type;
+                            delete obj._id
+                            $http.post(url,obj).then(
+                                function() {
+                                    $scope.progress.push('Saved ' + type + obj.name)
+                                    deferred.resolve()},
+                                function(err) {
+                                    $scope.progress.push('Failed ' + type + obj.name)
+                                    $scope.progress.push(angular.toJson(err.data))
+                                    deferred.reject()}
+                            )
+                            return deferred.promise
                         }
+
+
                     }
 
 
